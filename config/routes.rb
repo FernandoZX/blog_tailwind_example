776a8ac3a29115184dad3358ef91f072e0c75ba9
FernandoZX <ingfernandoc@gmail.com>
2022-09-13 23:13:59 -0500

    add missing gems, functionality and fix esbuild and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'public/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "articles#index"
+  root "public#index"
 
   resources :articles do
     resources :comments
